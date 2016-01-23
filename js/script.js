b760fa06abbb4137225f9cafb12e514b15dde6f9
Sonya Trachsel <sofyatrachsel@gmail.com>
2016-01-23 09:42:16 -0800

    Changed the script.js with the proper method to populate the json file

--- js/script.js
+++ js/script.js
@@ -7,67 +7,9 @@ $(document).ready(function(){
   var placeHolder = $('#content');
   // Fetch all portfolio items data from the server in JSON
   $.get('js/portfolioItems.json', function(data){
-    console.log ('get status???');
-    $(this).each(data, function(index, element){
-      // Generate the HTML for each post
-      var html = template(element);
+      // Generate the HTML for each post+
+    var html = template({portfolio:data});
       // Render the posts into the page
-      placeHolder.append(html);
-    }); // end each
+    placeHolder.append(html);
   }); // end get
 }); // end ready
-
-
-//
-//
-// $(document).ready(function(){
-//   var content = document.getElementById('content');
-//   var html = '';
-//   var data = [];
-//   var template = Handlebars.compile(document.getElementById('portfolio-template').innerHTML);
-//   content.innerHTML = template(data);
-  //
-  // $.ajax({
-  //   type: 'GET',
-  //   url: 'js/portfolioItems.json',
-  //   data: { get_param: 'value'},
-  //   dataType: 'json',
-  //   success: function (data) {
-  //     $('#portfolio-template').html('data');
-  //   }
-  // });
- //end ready
-
-
-
-  // var data = {
-  //   title: 'Portfolio',
-  //   portfolio: [
-  //     { title: 'WedWeek',
-  //       description: 'WedWeek is the website for couples who want to get married',
-  //       publishedOn: 'January 12th 2016',
-  //       category: 'Website',
-  //       img: './img/img1.jpg'
-  //     },
-  //     { title: 'Tokka Guide',
-  //       description: 'Tokka guide is an app to guide you around your city',
-  //       publishedOn: 'January 14th 2016',
-  //       category: 'IOS app',
-  //       img: './img/img2.jpg'
-  //     },
-  //     { title: 'Tokka Guide',
-  //       description: 'Tokka guide is an app to guide you around your city',
-  //       publishedOn: 'January 14th 2016',
-  //       category: 'IOS app',
-  //       img: './img/img3.jpg'
-  //     },
-  //     { title: 'Tokka Guide',
-  //       description: 'Tokka guide is an app to guide you around your city',
-  //       publishedOn: 'January 14th 2016',
-  //       category: 'IOS app',
-  //       img: './img/img4.jpg'
-  //     }]
-  // };
-
-
-  //
