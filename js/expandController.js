98c7e41d04028d157a5719762726b147af4b36a1
Sonya Trachsel <sofyatrachsel@gmail.com>
2016-02-04 21:33:35 -0800

    The modal windows now populate correct data

---
+++ js/expandController.js
@@ -0,0 +1,55 @@
+// (function(module){
+//   var expandController = {};
+//   expandController.index = function (){
+//   $('#resume').on('click', function(e){
+//     e.preventDefault();
+//     $('.isExpanded').show('slow', function(){
+//     console.log('woah')
+//   });//end show
+//   });//end click
+// };//end expandController
+//     module.expandController = expandController;
+//     })(window);
+
+// end combo
+//
+// $(document).on('click', "#viewProject a", function(e){
+//   e.preventDefault();
+//     $('#expanded ').show('slow', function(){
+//      console.log('woah')
+//     });
+//   });
+//
+// $(document).on('click', "#close", function(e){
+//     e.preventDefault();
+//       $('.isExpanded').hide('slow', function(){
+//        console.log('woah')
+//       });
+//     });
+
+//
+    $(document).on('click', '.viewProject a', function(e){
+        e.preventDefault();
+        // get specific project .expanded item.
+        // change from id of expanded to class.
+        var portfolioId = $(this).data('portfolio-id');
+        $('.expanded').hide();
+        $('.expanded-'+ portfolioId).show('slow', function() {
+          console.log('number')
+        });
+      });
+
+  // update to .close class.  target close class
+    $(document).on('click', '.close', function(e){
+        e.preventDefault();
+      // get specific project .expanded item.
+        $('.expanded').hide('slow', function(){
+         console.log('number')
+        });
+      });
+
+
+  // add inline style
+  // hide everything when page loads, show once clicked
+  //$(this).data('name')
+  //data() documentation
--- js/routes.js
+++ js/routes.js
@@ -2,4 +2,5 @@ page('/', homeController.index);
 page('/portfolio', portfolioController.index);
 page('/about-me', aboutController.index);
 page('/contact-me', contactController.index);
+// page('/about-me', expandController.index)
 page();
