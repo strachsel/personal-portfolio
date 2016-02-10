eac880d0dabecde3f78a9f981a4a934cdb50b673
Sonya Trachsel <sofyatrachsel@gmail.com>
2016-02-09 20:05:59 -0800

    updated get request in repo.js

--- js/repo.js
+++ js/repo.js
@@ -4,18 +4,14 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-
-    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
-    $.ajax({
-      url: 'https://api.github.com/users/brookr/repos',
-      type: 'GET',
-      headers: {'Authorization': 'token ' + githubToken },
-      success: function(data) {
-        repos.all = data;
-        callback();
-      }
-    })
-  };
+     $.get('/github/users/brookr/repos' +
+             '?per_page=100' +
+             '&sort=updated')
+       .done(function(data, message, xhr) {
+         repos.all = data;
+     })
+     .done(callback);
+   };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
