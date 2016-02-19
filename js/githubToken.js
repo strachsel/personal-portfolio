54525811630f32916d164b3f6281a7ef6b348d3e
Sonya Trachsel <sofyatrachsel@gmail.com>
2016-02-18 18:42:56 -0800

    deleted git integration

--- js/githubToken.js
+++
@@ -1,1 +0,0 @@
-var githubToken = '71d41d6ccf7cb659981e0e49671c273d5f1ffa21';
--- js/routes.js
+++ js/routes.js
@@ -2,6 +2,5 @@ page('/', homeController.index);
 page('/portfolio', portfolioController.index);
 page('/about-me', aboutController.index);
 page('/contact-me', contactController.index);
-page('/github', gitController.index);
 // page('/about-me', expandController.index)
 page();
