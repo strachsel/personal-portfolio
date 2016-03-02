b5dabe99894becc726d9c3e7d267c5c98e3b379c
Sonya Trachsel <sofyatrachsel@gmail.com>
2016-03-01 20:28:03 -0800

    added styles for the modal window +project elements

--- js/expand.js
+++ js/expand.js
@@ -1,57 +1,21 @@
-// (function(module){
-//   var expandController = {};
-//   expandController.index = function (){
-//   $('#resume').on('click', function(e){
-//     e.preventDefault();
-//     $('.isExpanded').show('slow', function(){
-//     console.log('woah')
-//   });//end show
-//   });//end click
-// };//end expandController
-//     module.expandController = expandController;
-//     })(window);
-
-// end combo
-//
-// $(document).on('click', "#viewProject a", function(e){
-//   e.preventDefault();
-//     $('#expanded ').show('slow', function(){
-//      console.log('woah')
-//     });
-//   });
-//
-// $(document).on('click', "#close", function(e){
-//     e.preventDefault();
-//       $('.isExpanded').hide('slow', function(){
-//        console.log('woah')
-//       });
-//     });
-
-//
-
-
-    $(document).on('click', '.viewProject a', function(e){
-        e.preventDefault();
-        // get specific project .expanded item.
-        // change from id of expanded to class.
-        var portfolioId = $(this).data('portfolio-id');
-        $('.modalDialog').hide();
-        $('.modalDialog-'+ portfolioId).show('slow', function() {
-        });
-        $('body').addClass('scrollprevent');
-      });
-
-  // update to .close class.  target close class
-    $(document).on('click', '.close', function(e){
-        e.preventDefault();
+$(document).ready(function(){
+  $(document).on('click', '.viewProject a', function(e){
+      e.preventDefault();
       // get specific project .expanded item.
-        $('.modalDialog').hide('slow', function(){
-        });
-        $('body').removeClass('scrollprevent');
+      // change from id of expanded to class.
+      var portfolioId = $(this).data('portfolio-id');
+      $('.modalDialog').hide();
+      $('.modalDialog-'+ portfolioId).show('slow', function() {
       });
+      $('body').addClass('scrollprevent');
+    });
 
-
-  // add inline style
-  // hide everything when page loads, show once clicked
-  //$(this).data('name')
-  //data() documentation
+// update to .close class.  target close class
+  $(document).on('click', '.close', function(e){
+      e.preventDefault();
+    // get specific project .expanded item.
+      $('.modalDialog').hide('slow', function(){
+      });
+      $('body').removeClass('scrollprevent');
+    });
+});
